599c1cfe452b88bd10fb16a317ca974c179bdca0
RaphaD <rduhen@ulb.ac.be>
2014-11-09 17:32:27 +0100

    * Beginning of the creation of the vue for an app calculating the BMI (Body Mass Index

--- src/com/example/Hello_Wrold/MyActivity.java
+++ src/com/example/Hello_Wrold/MyActivity.java
@@ -2,8 +2,10 @@ package com.example.Hello_Wrold;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
+
     /**
      * Called when the activity is first created.
      */
